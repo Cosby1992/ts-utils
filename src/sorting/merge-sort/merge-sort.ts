30549f966c06e814dc89d859b200c275f878e247
cosby1992 <andersbirkedahl@hotmail.com>
2022-10-11 01:04:41 +0200

    Fixed for making build possible without implementation

--- src/sorting/merge-sort/merge-sort.ts
+++ src/sorting/merge-sort/merge-sort.ts
@@ -4,6 +4,9 @@ export function sort<T>(array: T[]): T[] {
 
     const unsorted = [...array];
 
+    console.log(unsorted);
+    
+
     //TODO: Implement merge sort
 
 
@@ -11,10 +14,10 @@ export function sort<T>(array: T[]): T[] {
     return array;
 }
 
-function mergeSort<T>(array: T[], leftIndex: number, middleIndex: number, rightIndex: number) {
+export function mergeSort<T>(array: T[], leftIndex: number, middleIndex: number, rightIndex: number) {
 
     //TODO: Implement merge sort 
 
-
+    console.log(array, leftIndex, rightIndex, middleIndex);
 
 }
